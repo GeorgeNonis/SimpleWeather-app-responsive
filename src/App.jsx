d74caaf5f01e12ce9e02df6df5d2b432e1d65fda
GeorgeNonis <Georgenonis@gmail.com>
2023-01-11 17:25:54 +0100

    Added selection of choises for citys

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,88 @@ const App = () => {
   const [weather, setWeather] = useState("");
   const [error, setError] = useState(false);
   const [city, setCity] = useState("");
+  const [cities, setCities] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  axios
+    .get(
+      `http://api.openweathermap.org/data/2.5/weather?q=Larnaca&Municipality&appid=6ecf2fe9632179c173dbff234cc83200`
+    )
+    .then((res) => {
+      console.log(res.data);
+    });
+
+  // FETCHING START
   const fetchData = (city) => {
     setError(false);
     setLoading(true);
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6ecf2fe9632179c173dbff234cc83200`;
-    axios
-      .get(url)
-      .then((res) => {
-        setWeather(res.data);
-        console.log(res.data);
-      })
-      .catch((err) => {
-        setWeather("");
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=6ecf2fe9632179c173dbff234cc83200`;
+    axios.get(url).then((res) => {
+      if (res.data.length === 0) {
         setError(true);
-      });
+        setCities([]);
+        setWeather("");
+        return;
+      }
+      console.log(res.data);
+      setCities(res.data);
+    });
     setLoading(false);
   };
 
+  const fetchCity = (selectedCity) => {
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=6ecf2fe9632179c173dbff234cc83200`
+      )
+      .then((res) => {
+        setCities([]);
+        setWeather(res.data);
+        console.log(res.data);
+      });
+  };
+  // FETCHING END
+  // ***********************
+  // HANDLERS START
   const onKeyPress = (e) => {
     if (e.key === "Enter") {
       setCity(e.target.value);
-      return fetchData(e.target.value);
+      fetchData(e.target.value);
     }
   };
 
+  const onSelectCityHandler = (e) => {
+    let city = e.target.innerHTML.split(",")[0];
+    if (city.split(" ").length > 1) {
+      city = city.split(" ").join("&");
+    }
+    console.log(city);
+    fetchCity(city);
+  };
+  // HANDLERS END
+  // ***********************
+  // REMOVING DUBLICATE CITIES FROM ARRAY
+  let testing = [];
+  cities.map((city) => testing.push(`${city.name},${city.country}`));
+  const cleanup = Array.from(new Set(testing));
+  // ***********************
   return (
     <div className={styles.app}>
       <div className={styles.input_div}>
         <input type="text" name="weather" onKeyDown={onKeyPress} />
+        {cleanup && (
+          <div className={styles.cities_div}>
+            {cleanup.map((city, i) => (
+              <div
+                className={styles.cities}
+                key={i}
+                onClick={onSelectCityHandler}
+              >
+                {city}
+              </div>
+            ))}
+          </div>
+        )}
         <h3>{weather.name}</h3>
       </div>
       {loading && <div className={styles["loading-spinner"]}></div>}
@@ -65,7 +117,7 @@ const App = () => {
           </div>
           <div>
             <h3>Humidity</h3>
-            <h3>{weather.main.humidity}</h3>
+            <h3>{weather.main.humidity}%</h3>
           </div>
         </main>
       )}
