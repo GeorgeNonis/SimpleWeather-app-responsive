6872807483668e9ef9a2721959540c1be9e7818e
GeorgeNonis <Georgenonis@gmail.com>
2023-01-13 14:33:11 +0100

    Changes on the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import styles from "./App.module.scss";
+import { Cities } from "./components/cities";
+import { Error } from "./components/error";
+import { IsLoading } from "./components/loading";
+import { Weather } from "./components/weather";
 import useFetch from "./hooks/useFetch";
 
 const App = () => {
@@ -9,10 +13,10 @@ const App = () => {
   // ***********************
   // HANDLERS START
   const onKeyPress = (e) => {
-    if (e.key === "Enter") {
-      setCity(e.target.value);
-      fetchData(e.target.value);
-    }
+    if (e.key !== "Enter") return;
+
+    setCity(e.target.value);
+    fetchData(e.target.value);
   };
 
   const onSelectCityHandler = (e) => {
@@ -20,7 +24,7 @@ const App = () => {
     if (city.split(" ").length > 1) {
       city = city.split(" ").join("&");
     }
-    console.log(city);
+    console.log({ city });
     fetchCity(city);
   };
   // HANDLERS END
@@ -31,22 +35,6 @@ const App = () => {
   const cleanup = Array.from(new Set(testing));
   // ***********************
 
-  const validError = error && city.length > 0 && (
-    <h3 className={styles.error}>Cant find {city} in the database</h3>
-  );
-
-  const isLoading = loading && (
-    <div className={styles["loading-spinner"]}></div>
-  );
-  const citiesList = cleanup && (
-    <div className={styles.cities_div}>
-      {cleanup.map((city, i) => (
-        <div className={styles.cities} key={i} onClick={onSelectCityHandler}>
-          {city}
-        </div>
-      ))}
-    </div>
-  );
   return (
     <div className={styles.app}>
       <div className={styles.input_div}>
@@ -56,38 +44,12 @@ const App = () => {
           onKeyDown={onKeyPress}
           spellCheck={true}
         />
-        {citiesList}
+        <Cities obj={{ styles, onSelectCityHandler, cleanup }} />
         <h3>{weather && `${weather.name},${weather.sys.country}`}</h3>
       </div>
-      {isLoading}
-      {validError}
-      {weather && !error && (
-        <main className={styles.main_div}>
-          <div>
-            <h3>Description</h3>
-            <h3>
-              {weather.weather[0].description.charAt(0).toUpperCase() +
-                weather.weather[0].description.slice(1)}
-            </h3>
-          </div>
-          <div>
-            <h3>Temperature</h3>
-            <h3>{Math.round(weather.main.temp - 273.15)}°C</h3>
-          </div>
-          <div>
-            <h3>Feels like</h3>
-            <h3>{Math.round(weather.main.feels_like - 273.15)}°C</h3>
-          </div>
-          <div>
-            <h3>Wind speed</h3>
-            <h3>{weather.wind.speed} Mp/h</h3>
-          </div>
-          <div>
-            <h3>Humidity</h3>
-            <h3>{weather.main.humidity}%</h3>
-          </div>
-        </main>
-      )}
+      <IsLoading loading={loading} styles={styles} />
+      <Error error={error} styles={styles} city={city} />
+      <Weather error={error} styles={styles} weather={weather} />
     </div>
   );
 };
