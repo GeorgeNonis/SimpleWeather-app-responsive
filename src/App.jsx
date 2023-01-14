38857179b5247b9d283e53ca69bef756c32bc620
GeorgeNonis <Georgenonis@gmail.com>
2023-01-14 14:34:06 +0100

    remove the testing fnctions

--- src/App.jsx
+++ src/App.jsx
@@ -29,21 +29,6 @@ const App = () => {
 
   useDebounce(city, sendRequest);
 
-  // TESTING
-  axios
-    .get(
-      `http://api.openweathermap.org/data/2.5/weather?q=Ok,TM&appid=6ecf2fe9632179c173dbff234cc83200`
-    )
-    .then((res) => console.log(res.data));
-
-  axios
-    .get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=Ok&limit=5&appid=6ecf2fe9632179c173dbff234cc83200`
-    )
-    .then((res) => console.log(res.data));
-
-  // TESTING
-
   const onKeyPress = (e) => {
     if (e.key !== "Enter") return;
     sendRequest(sortCityName(city), true);
